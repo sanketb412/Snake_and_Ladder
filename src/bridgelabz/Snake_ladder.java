18428427293b651284b92c133714c51f265dd566
sanketb412 <sanketbagde38@gmail.com>
2021-04-27 19:20:06 +0530

    Added code for Rolliing a Dice from 1-6

--- src/bridgelabz/Snake_ladder.java
+++ src/bridgelabz/Snake_ladder.java
@@ -7,6 +7,10 @@ public class Snake_ladder {
 		System.out.println("#####  Welcome to Snake and Ladder Game  #####");
 		
 		System.out.println("\nYou are at a position '0'");
+		
+		double empCheck = (Math.floor((Math.random() * 10 ) % 6) + 1);
+		int IntValue = (int) Math.round(empCheck);
+		System.out.println("You Roll: " +IntValue);
 	}
 
 }
