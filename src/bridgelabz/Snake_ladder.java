8f11587e193fd2dfeb2bf79990ec83ea01864998
sanketb412 <sanketbagde38@gmail.com>
2021-04-27 18:56:28 +0530

    Added code for Welcoming

---
+++ src/bridgelabz/Snake_ladder.java
@@ -0,0 +1,10 @@
+package bridgelabz;
+
+public class Snake_ladder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("#####  Welcome to Snake and Ladder Game  #####");
+	}
+
+}
