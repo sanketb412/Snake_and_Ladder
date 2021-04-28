32954f75d350b705b053afb862a9734504687eeb
sanketb412 <sanketbagde38@gmail.com>
2021-04-28 11:40:42 +0530

    Added code for snake ladder position

--- src/bridgelabz/Snake_ladder.java
+++ src/bridgelabz/Snake_ladder.java
@@ -6,11 +6,42 @@ public class Snake_ladder {
 		// TODO Auto-generated method stub
 		System.out.println("#####  Welcome to Snake and Ladder Game  #####");
 		
-		System.out.println("\nYou are at a position '0'");
+		for (int i = 0; i <= 0; i++)
+		{
+			
+		System.out.println("\nYou are at a position '0' ");
+		int position = 0;
 		
-		double empCheck = (Math.floor((Math.random() * 10 ) % 6) + 1);
-		int IntValue = (int) Math.round(empCheck);
-		System.out.println("You Roll: " +IntValue);
+		double empCheck1 = (Math.floor((Math.random() * 10 ) % 6) + 1);
+		int IntValue1 = (int) Math.round(empCheck1);
+		System.out.println("You Roll: " +IntValue1);
+		System.out.println("player moves by: "+IntValue1);
+		
+		final int Ladder = 1;
+		final int Snake = 2;
+		double diecheck = (Math.floor(Math.random() * 10 ) % 3);
+		int IntValue2 = (int) Math.round(diecheck);
+		System.out.println("\n" +IntValue2);
+		
+		switch (IntValue2)
+		{
+      	case Ladder: 
+      		System.out.println("player move ahead through ladder");
+      		position += IntValue1; break;
+      	case Snake:
+      		System.out.println("player move behind through snake");
+      		position -= IntValue1; 
+      			if (position <=1 )
+      				position = 0; break;
+      	default:
+      		if (position <= 0)
+      			break;
+      		System.out.println("player doesnt move"); break;
+        }
+		
+		
+		System.out.println("now you are at  " +position);
+		}
 	}
 
 }
